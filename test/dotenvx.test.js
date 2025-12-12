33f24d6f9abc9a3b8a4db379e9e6aa78a2136fea
Mike Cunneen <mike@appworkshop.net>
2025-12-12 11:45:24 +0800

    ci: :construction_worker: .env: set github token for automatic releases
    also removed 'hello world' boilerplate

--- test/dotenvx.test.js
+++
@@ -1,8 +0,0 @@
-  import { assert } from "chai";
-
-  describe("dotenvx", () => {
-    it("should decrypt process.env.HELLO value", () => {
-      assert.equal(process.env.HELLO, "World");
-    });
-  });
-
---
+++ test/dummy.test.js
@@ -0,0 +1,8 @@
+  // import { assert } from "chai";
+
+  //TODO: remove once we have some real tests
+  describe("dummy", () => {
+    it("dummy", () => {
+    });
+  });
+
