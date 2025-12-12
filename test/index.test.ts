314c13c9c7c9241f6ad2d78abf53342c767e6bbe
Mike Cunneen <mike@appworkshop.net>
2025-12-12 19:42:57 +0800

    test: :white_check_mark: add boilerplate pino test

--- test/index.test.ts
+++ test/index.test.ts
@@ -3,12 +3,30 @@
 // give this a testy lookin filename, like
 // test/foo.js or ./lib/foo.test.js
 
+import { pino } from "pino";
+import { once, sink } from "pino-test";
 import { t } from "tap";
 import { greet } from "../src/index.ts";
 
 t.test("greet", async (t) => {
   const results = t.capture(console, "log");
   greet("hello");
-  t.match(results(), [{ args: ["hello"], returned: undefined }]); // don't have to call t.end(), it'll just end when the
+  t.match(results(), [{ args: ["hello"], returned: undefined }]);
+  // don't have to call t.end(), it'll just end when the
   // async stuff is all resolved.
+});
+
+t.test("pino", async (t) => {
+  const stream = sink();
+  const logger = pino(stream);
+  logger.info("hello world");
+
+  const expected = { msg: "hello world", level: 30 };
+  await once(stream, expected);
+
+  logger.info("hello world 1");
+
+  await once(stream, (log: Record<string, unknown>) => {
+    t.equal(log.msg, "hello world 1");
+  });
 });
